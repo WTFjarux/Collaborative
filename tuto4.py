234957bf706ae93df1b00d3e412e3224a2d7d263
Pravesh <lamatmzpravesh@gmail.com>
2025-03-19 08:06:58 +0545

    Added new Python script

---
+++ tuto4.py
@@ -0,0 +1,5 @@
+def subtract_numbers():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    return f"Result: {num1 - num2}"
+print(subtract_numbers())
